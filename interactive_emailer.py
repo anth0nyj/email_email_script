051022e6836b61909920ed00eada8d28b5db2345
Anthony Alvarez <ajtalvarez@gmail.com>
2019-05-03 20:36:16 -0400

    Refactored to remove cruft, added helpful comments.

--- interactive_emailer.py
+++ interactive_emailer.py
@@ -2,58 +2,61 @@
 import smtplib
 import getpass
 from pathlib import Path
+import pdb
+
 
 # Get Login Credentials
 
-def createToken(**kwargs):
-    if kwargs:
-        return kwargs['file_creds']
-    else:
-        token = dict({'name': None, 'email_addr': None, 'pw': None})
-        token.update({
-            'name': input("Enter your name: "),
-            'email_addr': input("Enter your email address: "),
-            'pw': getpass.getpass("Enter your password: ")
-        })
-        return token
+def createToken():
+    from logintoken import LoginToken
+    name = input("Enter your name: ")
+    addr = input("Enter your email address: ")
+    pw = getpass.getpass("Enter your password: ")
+    token = LoginToken(addr, pw, name)
     return token
 
-
-def obtainCreds():
+# Pull credentials from local file if possible. Otherwise, prompt cred entry.
+def obtainToken():
+    token = None
+    # First, try to find and use cred file.
     try:
-        login_creds = Path("./login_creds")
-        if login_creds:
-            from login_creds import LoginCreds
-            file_creds = LoginCreds()
-            use_file_response = None
-            while (use_file_response != 'y' and use_file_response != 'n'):
-                use_file_response = input("A login credential file for \"" + file_creds.email_addr + "\"  has been found. Would you like to use this to log in? [y/n]: ")
-                if use_file_response == 'y':
-                    token = createToken(file_creds=file_creds)
-                    return token
-                elif use_file_response == 'n':
+        if Path("./login_creds").exists:
+            from login_creds import login_token
+            # Give user option to use file or manually enter creds.
+            use_file_res = None
+            while (use_file_res != 'y' and use_file_res != 'n'):
+                use_file_res = input("A login credential file for \"" + getattr(login_token, 'addr') + "\"  has been found. Would you like to use this to log in? [Y/n]: ").lower() or 'y'
+                if use_file_res.lower() == 'y':
+                    return login_token
+                elif use_file_res.lower() == 'n':
                     token = createToken()
                     return token
                 else:
                     print("Please submit a valid response.")
             return token
         return token
+
+    # Cred file not found. Prompt cred entry.
     except:
         print("No login credential file found.")
+        return createToken()
+
     if token:
         return token
 
-creds = obtainCreds()
+token = obtainToken()
 
 # Start/Configure SMTP Server
+print("Initializing server...")
 server = smtplib.SMTP('smtp.gmail.com', 587)
+print("Connecting server...")
 server.connect("smtp.gmail.com", 587)
 server.ehlo()
 server.starttls()
 server.ehlo()
 
 # Use credentials to login
-server.login(getattr(creds, 'email_addr'), getattr(creds, 'pw'))
+server.login(getattr(token, 'addr'), getattr(token, 'pw'))
 
 # Compose email
 rec_addr = input("Enter recipient email: ")
@@ -61,14 +64,14 @@ msg_subj = input("Enter a subject line: ")
 msg_body = input("Enter the body of your email: ")
 
 # Format input to create email
-msg = (f"""From: {getattr(creds, 'name')} <{getattr(creds, 'email_addr')}>
+msg = (f"""From: {getattr(token, 'name')} <{getattr(token, 'addr')}>
 To: <{rec_addr}>
 Subject: {msg_subj}
 
 {msg_body}""")
 
 # Send email
-server.sendmail(getattr(creds, 'email_addr'), rec_addr, msg)
+server.sendmail(getattr(token, 'addr'), rec_addr, msg)
 
 # Close server
 server.quit()
